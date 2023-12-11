bcc948e818edf062a492a6816028ffba03139773
AATMAN BORDA <aatmanb@koholint.cs.wisc.edu>
2023-12-11 11:46:08 -0600

    added compressors in config file and more benchmarks

--- configs/example/spec06_benchmarks.py
+++ configs/example/spec06_benchmarks.py
@@ -24,7 +24,7 @@ gcc.executable = bench_dir + 'gcc' + x86_suffix
 #gcc.cmd = [gcc.executable] + ['expr2.i', '-o', 'expr2.s']
 #gcc.cmd = [gcc.executable] + ['g23.i', '-o', 'g23.s']
 #gcc.cmd = [gcc.executable] + ['s04.i', '-o', 's04.s']
-gcc.cmd = [gcc.executable] + ['scilab.i', '-o', 'scilab.s']
+gcc.cmd = [gcc.executable] + [bench_dir + 'input/scilab.i', '-o', bench_dir + 'scilab.s']
 #gcc.output = out_dir + 'gcc.out'
 
 #437.leslie3d
@@ -56,7 +56,7 @@ omnetpp.executable = bench_dir + 'omnetpp' + x86_suffix
 # TEST CMDS
 #omnetpp.cmd = [omnetpp.executable] + ['omnetpp.ini']
 # REF CMDS
-omnetpp.cmd = [omnetpp.executable] + ['omnetpp.ini']
+omnetpp.cmd = [omnetpp.executable] + [bench_dir + 'omnetpp.ini']
 #omnetpp.output = out_dir + 'omnetpp.out'
 
 #450.soplex
@@ -66,6 +66,60 @@ soplex.executable = bench_dir + 'soplex' + x86_suffix
 # TEST CMDS
 #soplex.cmd = [soplex.executable] + ['-m10000', 'test.mps']
 # REF CMDS
-soplex.cmd = [soplex.executable] + ['-s1', '-e', '-m45000', 'pds-50.mps']
+soplex.cmd = [soplex.executable] + ['-s1', '-e', '-m45000', bench_dir + 'pds-50.mps']
 #soplex.cmd = [soplex.executable] + ['-m3500', 'ref.mps']
 #soplex.output = out_dir + 'soplex.out'
+
+#400.perlbench
+perlbench = Process() # Update June 7, 2017: This used to be LiveProcess()
+bench_dir = spec2006_dir + 'perlbench/'
+perlbench.executable = bench_dir + 'perlbench' + x86_suffix
+# TEST CMDS
+#perlbench.cmd = [perlbench.executable] + ['-I.', '-I./lib', 'attrs.pl']
+# REF CMDS
+perlbench.cmd = [perlbench.executable] + ['-I'+bench_dir+'lib', 'checkspam.pl', '2500', '5', '25', '11', '150', '1', '1', '1', '1']
+#perlbench.cmd = [perlbench.executable] + ['-I./lib', 'diffmail.pl', '4', '800', '10', '17', '19', '300']
+#perlbench.cmd = [perlbench.executable] + ['-I./lib', 'splitmail.pl', '1600', '12', '26', '16', '4500']
+#perlbench.output = out_dir+'perlbench.out'
+ 
+#429.mcf
+mcf = Process() # Update June 7, 2017: This used to be LiveProcess()
+bench_dir = spec2006_dir + 'mcf/'
+mcf.executable = bench_dir + 'mcf' + x86_suffix
+# TEST CMDS
+#mcf.cmd = [mcf.executable] + ['inp.in']
+# REF CMDS
+mcf.cmd = [mcf.executable] + [bench_dir + 'inp.in']
+#mcf.output = out_dir + 'mcf.out'
+ 
+#433.milc
+milc = Process() # Update June 7, 2017: This used to be LiveProcess()
+bench_dir = spec2006_dir + 'milc/'
+milc.executable = bench_dir +'milc' + x86_suffix
+# TEST CMDS
+#milc.cmd = [milc.executable]
+#milc.input = 'su3imp.in'
+# REF CMDS
+milc.cmd = [milc.executable]
+milc.input = bench_dir + 'input/su3imp.in'
+#milc.output = out_dir + 'milc.out'
+ 
+#459.GemsFDTD
+GemsFDTD = Process() # Update June 7, 2017: This used to be LiveProcess()
+bench_dir = spec2006_dir + 'GemsFDTD/'
+GemsFDTD.executable = bench_dir +'GemsFDTD' + x86_suffix 
+# TEST CMDS
+#GemsFDTD.cmd = [GemsFDTD.executable]
+# REF CMDS
+GemsFDTD.cmd = [GemsFDTD.executable]
+#GemsFDTD.output = out_dir + 'GemsFDTD.out'
+
+#470.lbm
+lbm = Process() # Update June 7, 2017: This used to be LiveProcess()
+bench_dir = spec2006_dir + 'lbm/'
+lbm.executable = bench_dir +'lbm' + x86_suffix
+# TEST CMDS
+#lbm.cmd = [lbm.executable] + ['20', 'reference.dat', '0', '1', '100_100_130_cf_a.of']
+# REF CMDS
+lbm.cmd = [lbm.executable] + ['300', bench_dir+'reference.dat', '0', '0', bench_dir+'100_100_130_ldc.of']
+#lbm.output = out_dir + 'lbm.out'
